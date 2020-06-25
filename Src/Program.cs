e6430148167e839a90e475dd6c02117d8edf6ae4
DemetrioQ <52639107+DemetrioQ@users.noreply.github.com>
2020-06-25 17:42:18 -0400

    Complete Program.cs

--- Src/Program.cs
+++ Src/Program.cs
@@ -15,14 +15,15 @@ namespace dummy_anagrams.Src
             Anagrams anagram = new Anagrams();
             anagram.Print(diccionary.anagrams);
             timer.Stop();
+            Console.WriteLine();
             Console.WriteLine($"Cantidad de tiempo: {timer.Elapsed}");
             Console.WriteLine($"Cantidad de palabras: {file.get_array().Length}");
             Console.WriteLine($"Cantidad de sets de anagramas: {anagram.anagrams_sets_quantity}");
             Console.WriteLine($"Cantidad de palabras con anagramas: {anagram.anagram_words_count}");
             anagram.ObtainLongestSet();
             anagram.ObtainLongestWord();
-            Console.WriteLine($"Set de anagramas con mas palabras: {string.Join(", ", anagram.longests_set)} \n Tamano: {anagram.longests_set.Count}");
-            Console.WriteLine($"Set de anagramas con palabras mas largas: {string.Join(", ", anagram.longests_words)} \n Tamano: {anagram.longest_key.Length}");
+            Console.WriteLine($"Set de anagramas con mas palabras: {string.Join(", ", anagram.longests_set)} \nTamaño: {anagram.longests_set.Count}");
+            Console.WriteLine($"Set de anagramas con palabras mas largas: {string.Join(", ", anagram.longests_words)} \nTamaño: {anagram.longest_key.Length}");
 
         }
     }
