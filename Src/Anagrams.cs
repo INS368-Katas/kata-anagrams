55c725f0d0ee860e6dfbf3e70ebabe7ec3cb6bce
DemetrioQ <52639107+DemetrioQ@users.noreply.github.com>
2020-06-26 18:22:26 -0400

    Now prints to console faster

--- Src/Anagrams.cs
+++ Src/Anagrams.cs
@@ -1,56 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Diagnostics;
+using System.IO;
 
 namespace dummy_anagrams.Src
 {
     public class Anagrams
     {
-        public Dictionary<string, List<String>> anagram_sets = new Dictionary<string, List<string>>();
+        public Dictionary<string, List<string>> anagrams;
         public string longest_key = "";
-        public int anagram_words_count = 0;
-        public int anagrams_sets_quantity = 0;
-        public List<string> longests_words = new List<string>();
         public List<string> longests_set = new List<string>();
-        public void Print(Dictionary<string, List<String>> map)
-        {
+        public List<string> longests_words = new List<string>();
+        public int anagrams_sets_quantity = 0;
+        public int anagram_words_count = 0;
 
-            foreach (var set in map)
-            {
-                if (set.Value.Count > 1)
-                {
-                    anagram_words_count += set.Value.Count;
-                    anagrams_sets_quantity++;
-                    anagram_sets.Add(set.Key, set.Value);
-                    Console.WriteLine(string.Join(", ", set.Value));
-                }
-            }
+        public Anagrams(Hashing dic)
+        {
+            anagrams = dic.anagrams;
         }
-
-        public void ObtainLongestWord()
+        public void Print(TextWriter sw)
         {
-            foreach (var set in anagram_sets)
+            foreach (var set in anagrams)
             {
+                anagram_words_count += set.Value.Count;
+                anagrams_sets_quantity++;
+                sw.WriteLine(string.Join(", ", set.Value));
+
                 if (set.Key.Length > longest_key.Length)
-                {
-                    longests_words = set.Value;
                     longest_key = set.Key;
-                }
-            }
-        }
-        public void ObtainLongestSet()
-        {
-            foreach (var set in anagram_sets)
-            {
-                if (set.Value.Count > longests_set.Count)
-                {
 
+                if (set.Value.Count > longests_set.Count)
                     longests_set = set.Value;
 
-                }
+                longests_words = anagrams[longest_key];
             }
         }
-
 
     }
 }
--- Src/Program.cs
+++ Src/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 
 namespace dummy_anagrams.Src
 {
@@ -8,25 +9,32 @@ namespace dummy_anagrams.Src
     {
         static void Main(string[] args)
         {
+            Console.WindowWidth = 165;
+
             Stopwatch timer = new Stopwatch();
             timer.Start();
             Archivo file = new Archivo("wordlist.txt");
             Hashing diccionary = new Hashing(file.get_array());
-            Anagrams anagram = new Anagrams();
-
-            anagram.Print(diccionary.anagrams);
+            Anagrams anagram = new Anagrams(diccionary);
+            
+            TextWriter sw = new StreamWriter(Console.OpenStandardOutput());
+            Console.SetOut(sw);
+            anagram.Print(sw);
             timer.Stop();
+
 
             Console.WriteLine();
             Console.WriteLine($"Cantidad de tiempo: {timer.Elapsed}");
-            Console.WriteLine($"Cantidad de palabras: {file.get_array().Length}");
+            Console.WriteLine($"Cantidad de palabras en este archivo: {file.get_array().Length}");
             Console.WriteLine($"Cantidad de sets de anagramas: {anagram.anagrams_sets_quantity}");
             Console.WriteLine($"Cantidad de palabras con anagramas: {anagram.anagram_words_count}");
 
-            anagram.ObtainLongestSet();
-            anagram.ObtainLongestWord();
-            Console.WriteLine($"Set de anagramas con mas palabras: {string.Join(", ", anagram.longests_set)} \nTamaño: {anagram.longests_set.Count}");
-            Console.WriteLine($"Set de anagramas con palabras mas largas: {string.Join(", ", anagram.longests_words)} \nTamaño: {anagram.longest_key.Length}");
+            Console.WriteLine($"Set de anagramas con mas palabras: {string.Join(", ", anagram.longests_set)} \nTamano: {anagram.longests_set.Count}");
+            Console.WriteLine($"Set de anagramas con palabras mas largas: {string.Join(", ", anagram.longests_words)} \nTamano: {anagram.longest_key.Length}");
+
+            sw.Flush();
+            Console.ReadKey();
+            
 
         }
     }
